a0a047badd9425c74fcc63ffb3fdf73a344eb5d0
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-12-27 20:51:25 +0200

    add-start-mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,52 @@
-const express = require('express')
+import fs from "fs/promises";
+import path from "path";
 
-const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactsPath = path.resolve("models", "contacts.json",);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const updateList = (contacts) => { fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)) };
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+export async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;
+};
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) { 
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  updateList(contacts);
+  return result;
+};
 
-module.exports = router
+export async function addContact(data) {
+
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  updateList(contacts);
+  return newContact;
+};
+
+export async function updateContact(contactId, data) { 
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...contacts[index], ...data };
+  updateContact(contacts);
+  return contacts[index];
+};
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+import { mongoose } from 'mongoose';
+import app from './app.js'
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://Dima:V4QGuv9T7pkkxbeA@cluster0.bwi6msn.mongodb.net/';
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch(error => {
+    console.log(error.mongoose);
+    process.exit(1);
+  });
+
+
+// MongoDB: pass:V4QGuv9T7pkkxbeA login:Dima;
