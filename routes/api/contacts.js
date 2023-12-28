25d22799154fdd401f6210277423bd4696d7d807
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-12-28 19:49:56 +0200

    add-start-mongoose-and-files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,52 +1,19 @@
-import fs from "fs/promises";
-import path from "path";
+import { Router } from 'express';
+import controller from '../../controllers/controller.js';
+import { isEmptyBody } from "../../middlewares/index.js";
+
+const router = Router();
+
+router.get('/', controller.getAll);
+
+router.get('/:contactId', controller.getById);
+
+router.post('/', isEmptyBody, controller.add);
+
+router.put('/:contactId', isEmptyBody, controller.updateById);
+
+router.delete('/:contactId', controller.removeContact );
 
 
-const contactsPath = path.resolve("models", "contacts.json",);
 
-const updateList = (contacts) => { fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)) };
-
-export async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
-};
-
-export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const result = contacts.find(item => item.id === contactId);
-  return result || null;
-};
-
-export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(item => item.id === contactId);
-  if (index === -1) { 
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  updateList(contacts);
-  return result;
-};
-
-export async function addContact(data) {
-
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    ...data,
-  };
-  contacts.push(newContact);
-  updateList(contacts);
-  return newContact;
-};
-
-export async function updateContact(contactId, data) { 
-  const contacts = await listContacts();
-  const index = contacts.findIndex(item => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { ...contacts[index], ...data };
-  updateContact(contacts);
-  return contacts[index];
-};
+export default router;
