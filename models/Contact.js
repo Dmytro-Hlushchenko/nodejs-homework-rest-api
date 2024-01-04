6a40d39780d6711d508e6f25c6a6d622e4c8095b
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2024-01-04 21:36:46 +0200

    fix-contactsAddSchema

--- models/Contact.js
+++ models/Contact.js
@@ -7,7 +7,6 @@ export const contactsAddSchema = Joi.object(
         name: Joi.string().required(),
         email: Joi.string().required(),
         phone: Joi.string().required(),
-        favorite: Joi.boolean().required(),
     }
 );
 
